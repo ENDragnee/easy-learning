ac0b809e6839ae651ec652c5f25bd0df1106a84f
Mastwal Mesfin <126337103+ENDragnee@users.noreply.github.com>
2024-12-11 00:11:37 +0300

    Some UI changes

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -193,46 +193,30 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, setIsOpen }) => {
           </svg>
           Flash Cards
         </Button>
-      ) : (
-        <Button
-          variant="ghost"
-          className="flex items-center justify-center p-2 w-full hover:bg-[#4b5162] hover:text-white"
-          onClick={() => router.push("/flashcard")}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z" />
-            <path fillRule="evenodd" d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z" clipRule="evenodd" />
-          </svg>
-        </Button>
-      )}
+      ) : ("")}
       </>
       <div className="mt-auto mb-4 p-2">
-  {session?.user?.name && (
-    <div className="relative">
-      <button
-        onClick={() => setIsUserMenuOpen(!isUserMenuOpen)}
-        className={cn(
-          "flex items-center w-full",
-          isOpen ? "justify-start px-2" : "justify-center"
-        )}
-      >
-        <div className={cn(
-          "w-8 h-8 rounded-full bg-blue-500 flex items-center justify-center text-white font-semibold",
-          !isOpen && "w-6 h-6 text-sm"
-        )}>
-          {session.user.name.charAt(0).toUpperCase()}
-        </div>
-        {isOpen && (
-          <span className="ml-2 text-sm font-medium">
-            {session.user.name}
-          </span>
-        )}
-      </button>
+        {session?.user?.name && (
+        <div className="relative">
+          <button
+            onClick={() => setIsUserMenuOpen(!isUserMenuOpen)}
+            className={cn(
+              "flex items-center w-full",
+              isOpen ? "justify-start px-2" : "md:justify-center hidden"
+            )}
+          >
+            <div className={cn(
+            "w-8 h-8 rounded-full bg-blue-500 flex items-center justify-center text-white font-semibold",
+            !isOpen && "w-6 h-6 text-sm"
+          )}>
+            {session.user.name.charAt(0).toUpperCase()}
+          </div>
+          {isOpen && (
+            <span className="ml-2 text-sm font-medium">
+              {session.user.name}
+            </span>
+          )}
+        </button>
       
       {isUserMenuOpen && isOpen && (
       <div className={cn(
